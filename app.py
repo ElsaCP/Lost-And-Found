277f07b9718770a85c80b5e421c066baa061a51a
Auliaah30 <auliaah10@gmail.com>
2025-11-27 16:30:00 +0700

    [+] add update router

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 # app.py
 from flask import Flask
+from config import Config
+from extensions import db
 from routes.route import main
 from routes.admin_route import admin_bp
 
 app = Flask(__name__)
-app.secret_key = 'rahasia_elsa'  # penting untuk session
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
 
+# Membuat tabel jika belum ada
+with app.app_context():
+    db.create_all()  # Membuat semua tabel berdasarkan model
+    
 # Daftarkan blueprint
 app.register_blueprint(main)
 app.register_blueprint(admin_bp, url_prefix='/admin')  # tambahkan prefix
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+    
--- config.py
+++ config.py
@@ -1,12 +1,19 @@
 # config.py
 import mysql.connector
 from mysql.connector import Error
+import os
 
 def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
-        user="root",      # sesuaikan
-        password="",      # sesuaikan
+        user="root",
+        password="",
         database="lostfound",
         charset='utf8mb4'
     )
+
+class Config:
+    # Gunakan database yang kamu pakai (lostfound atau inj_db)
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@127.0.0.1:3306/lostfound?charset=utf8mb4"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = "supersecretkey123"
